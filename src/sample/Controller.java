fcc1b0a0d7587f15577ae87f4aa3b9b064c45dea
BRAMBLE-REACH-S\lenna <lennart.cockx@gmail.com>
2017-03-20 20:35:04 +0100

    moved launching method to launcher class added description for Launcher class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,40 +1,25 @@
 package sample;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
-
-import java.io.IOException;
 
 public class Controller {
 
     private Downloader con = new Downloader();
+    private Launcher launch = new Launcher();
 
     @FXML
     private TextField installLoc;
 
     @FXML
     private void launchHandle(){
-        launchMinecraft(installLoc.getText());
-    }
-
-    private void launchMinecraft(String location) {
-        try {
-            Runtime.getRuntime().exec("java -jar " + location +"launcher.jar" );
-            Platform.exit();
-            System.exit(0);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        launch.launchMinecraft(installLoc.getText());
     }
 
     @FXML
     protected void initialize(){
         //set install location to appdata/.minecraft
-        System.out.println("Application loaded");
         installLoc.setText(System.getenv("Appdata")+"\\.minecraft\\");
-
     }
 
 }
--- src/sample/Launcher.java
+++ src/sample/Launcher.java
@@ -1,8 +1,25 @@
 package sample;
 
+import javafx.application.Platform;
+
+import java.io.IOException;
+
 /**
  * Created by lennart on 3/20/2017.
+ * This class will handle launching minecraft and related functions
  */
-public class Launcher {
 
+
+class Launcher {
+
+    void launchMinecraft(String location) {
+        try {
+            Runtime.getRuntime().exec("java -jar " + location +"launcher.jar" );
+            Platform.exit();
+            System.exit(0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
