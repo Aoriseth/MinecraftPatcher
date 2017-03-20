c421e79727417aa3510f03a28697ddf9cef91ced
BRAMBLE-REACH-S\lenna <lennart.cockx@gmail.com>
2017-03-20 20:38:51 +0100

    added description for Downloader class added fileList check to prevent nullpointerexception added print if mod folder already exists

--- src/sample/Downloader.java
+++ src/sample/Downloader.java
@@ -4,19 +4,22 @@ import java.io.File;
 
 /**
  * Created by lennart on 3/20/2017.
+ * This class will contain methods to download files via ftp and determine locally installed mods
  */
 public class Downloader {
-    public Downloader() {
+    Downloader() {
     }
 
     public File[] getLocalFiles(String dir) {
         File localFolder = new File(dir);
-        localFolder.mkdir();
+        boolean success = localFolder.mkdir();
+        if(!success){
+            System.out.println("Mod folder already exists");
+        }
         File[] filesList = localFolder.listFiles();
-        for (File file : filesList) {
-            if (file.isFile()) {
-                System.out.println(file.getName());
-            }
+        if (filesList != null) {
+            for (File file : filesList)
+                if (file.isFile()) System.out.println(file.getName());
         }
         return filesList;
     }
