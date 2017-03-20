b58535aa12c88c449e06d7f3301427bfe0d668e4
BRAMBLE-REACH-S\lenna <lennart.cockx@gmail.com>
2017-03-20 20:22:14 +0100

    application can now detect and launch minecraft application will minimize on launch and maximize on close

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,9 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
 
@@ -15,7 +18,22 @@ public class Controller {
 
     @FXML
     private void launchHandle(){
-        con.getLocalFiles(installLoc.getText()+"mods\\");
+        launchMinecraft(installLoc.getText());
+    }
+
+    private void launchMinecraft(String location) {
+        try {
+            Process proc = Runtime.getRuntime().exec("java -jar " + location +"launcher.jar" );
+            ((Stage)launchButton.getScene().getWindow()).setIconified(true);
+            proc.waitFor();
+            ((Stage)launchButton.getScene().getWindow()).setIconified(false);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.print("failed");
+        }
     }
 
     @FXML
